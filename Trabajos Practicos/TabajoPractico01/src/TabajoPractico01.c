d724833046b1c7ee3109e538831063896b00ca16
Dante Saldivar <dantexsaldivar@gmail.com>
2022-04-04 16:08:48 -0300

    Prueba Tp

---
+++ Trabajos Practicos/TabajoPractico01/src/TabajoPractico01.c
@@ -0,0 +1,15 @@
+/*
+ ============================================================================
+DanteSaldivar
+Division H
+Trabajo Practico 01
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
